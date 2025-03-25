7c32ac4cb3b7092cfe3aa55d1ac753afe066cc04
DAM1 <mario.ganfer@educa.jcyl.es>
2025-03-25 09:43:19 +0100

    Finalizado a falta de revisar

--- app/src/main/java/com/example/juegoparejas/MainActivity.kt
+++ app/src/main/java/com/example/juegoparejas/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.juegoparejas
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -135,5 +141,174 @@ class MainActivity : AppCompatActivity() {
                 mp.start()
             }
         }
+    }
+
+    fun musicaFondo(v:View){
+        if(escuchar==true){
+            mpFondo.pause()
+            ib_sonido.setImageResource(R.drawable.ic_volume_off)
+            ib_sonido.setColorFilter(Color.GRAY)
+        } else {
+            mpFondo.start()
+            ib_sonido.setImageResource(R.drawable.ic_volumen_on)
+            ib_sonido.setColorFilter(Color.GREEN)
+        }
+        escuchar = !escuchar
+    }
+
+    fun seleccionar(imagen: View){
+        sonido("touch")
+
+        verificar(imagen)
+    }
+
+    private fun verificar(imagen: View) {
+        var iv = (imagen as ImageView)
+        var tag = imagen.tag.toString().toInt()
+        if (imagenesArray[tag] == 11) {
+            imagen.setImageResource(mario)
+        } else if (imagenesArray[tag] == 12) {
+            imagen.setImageResource(luigi)
+        } else if (imagenesArray[tag] == 13) {
+            imagen.setImageResource(yoshi)
+        } else if (imagenesArray[tag] == 14) {
+            imagen.setImageResource(toad)
+        } else if (imagenesArray[tag] == 15) {
+            imagen.setImageResource(peach)
+        } else if (imagenesArray[tag] == 16) {
+            imagen.setImageResource(wario)
+        } else if (imagenesArray[tag] == 21) {
+            imagen.setImageResource(mario)
+        } else if (imagenesArray[tag] == 22) {
+            imagen.setImageResource(luigi)
+        } else if (imagenesArray[tag] == 23) {
+            imagen.setImageResource(yoshi)
+        } else if (imagenesArray[tag] == 24) {
+            imagen.setImageResource(toad)
+        } else if (imagenesArray[tag] == 25) {
+            imagen.setImageResource(peach)
+        } else if (imagenesArray[tag] == 26) {
+            imagen.setImageResource(wario)
+        }
+        //Guardar temporalmente imagen seleccionada
+        if (numeroImagen == 1) {
+            imagen1 = iv
+            numeroImagen = 2
+            iv.isEnabled = false
+        } else if (numeroImagen == 2) {
+            imagen2 = iv
+            numeroImagen = 1
+            iv.isEnabled = false
+            deshabilitarImagenes()
+            val h = Handler(Looper.getMainLooper())
+            h.postDelayed({sonImagenesIguales()}, 1000)
+        }
+
+    }
+
+    private fun sonImagenesIguales() {
+        if(imagen1.drawable.constantState == imagen2.drawable.constantState){
+            sonido("success")
+            if(turno == 1){
+                puntosj1++
+                tv_j1.text = "J1: $puntosj1"
+            } else if(turno == 2){
+                puntosj2++
+                tv_j2.text = "J2: $puntosj2"
+            }
+            imagen1.isEnabled = false
+            imagen2.isEnabled = false
+            imagen1.tag = ""
+            imagen2.tag = ""
+        } else {
+            sonido("no")
+            imagen1.setImageResource(R.drawable.oculta)
+            imagen2.setImageResource(R.drawable.oculta)
+            if(turno == 1){
+                turno = 2
+                tv_j1.setTextColor(Color.GRAY)
+                tv_j2.setTextColor(Color.WHITE)
+            } else if(turno == 2){
+                turno = 1
+                tv_j1.setTextColor(Color.WHITE)
+                tv_j2.setTextColor(Color.GRAY)
+            }
+        }
+        iv_11.isEnabled = !iv_11.tag.toString().isEmpty()
+        iv_12.isEnabled = !iv_12.tag.toString().isEmpty()
+        iv_13.isEnabled = !iv_13.tag.toString().isEmpty()
+        iv_14.isEnabled = !iv_14.tag.toString().isEmpty()
+        iv_21.isEnabled = !iv_21.tag.toString().isEmpty()
+        iv_22.isEnabled = !iv_22.tag.toString().isEmpty()
+        iv_23.isEnabled = !iv_23.tag.toString().isEmpty()
+        iv_24.isEnabled = !iv_24.tag.toString().isEmpty()
+        iv_31.isEnabled = !iv_31.tag.toString().isEmpty()
+        iv_32.isEnabled = !iv_32.tag.toString().isEmpty()
+        iv_33.isEnabled = !iv_33.tag.toString().isEmpty()
+        iv_34.isEnabled = !iv_34.tag.toString().isEmpty()
+
+        verificarFinJuego()
+
+    }
+
+    private fun verificarFinJuego() {
+        if (
+            iv_11.tag.toString().isEmpty() &&
+            iv_12.tag.toString().isEmpty() &&
+            iv_13.tag.toString().isEmpty() &&
+            iv_14.tag.toString().isEmpty() &&
+            iv_21.tag.toString().isEmpty() &&
+            iv_22.tag.toString().isEmpty() &&
+            iv_23.tag.toString().isEmpty() &&
+            iv_24.tag.toString().isEmpty() &&
+            iv_31.tag.toString().isEmpty() &&
+            iv_32.tag.toString().isEmpty() &&
+            iv_33.tag.toString().isEmpty() &&
+            iv_34.tag.toString().isEmpty()
+            ){
+                mp.stop()
+                mp.release()
+            sonido("win")
+            val  builder = AlertDialog.Builder(this)
+            builder
+                .setTitle("FIN DEL JUEGO")
+                .setMessage("PUNTAJE \nJ1"+puntosj1+"\nJ2"+puntosj2)
+                .setCancelable(false)
+                .setPositiveButton("Jugar de nuevo",
+                    DialogInterface.OnClickListener{dialogInterface, i ->
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    })
+                .setNegativeButton("Salir",
+                    DialogInterface.OnClickListener{dialogInterface, i ->
+                        finish()
+                    })
+            val ad = builder.create()
+            ad.show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mpFondo.stop()
+        mpFondo.release()
+    }
+
+    private fun deshabilitarImagenes() {
+        iv_11.isEnabled = false
+        iv_12.isEnabled = false
+        iv_13.isEnabled = false
+        iv_14.isEnabled = false
+        iv_21.isEnabled = false
+        iv_22.isEnabled = false
+        iv_23.isEnabled = false
+        iv_24.isEnabled = false
+        iv_31.isEnabled = false
+        iv_32.isEnabled = false
+        iv_33.isEnabled = false
+        iv_34.isEnabled = false
+
+
     }
 }
